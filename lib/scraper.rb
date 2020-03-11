a885f581ce3e1e2a4a8cd79dbd6082ab871e18d3
Doak Covington <doakcovington@protonmail.com>
2020-03-11 02:30:00 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -34,7 +34,7 @@ class Scraper
         student_profile[:blog] = links
       end
     end#end of div.social-icon-container
-    student_profile[:bio] = doc.css("div.main-wrapper.profile .vitals-text-container .profile-quote").text
+    student_profile[:profile-quote] = doc.css("div.main-wrapper.profile .vitals-text-container .profile-quote").text
     student_profile
   end
 

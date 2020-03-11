2268631b3371364971fea4fd1e16ba92ca6a0774
Doak Covington <doakcovington@protonmail.com>
2020-03-11 02:42:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,6 +8,7 @@ class Student
   def initialize(student_hash)
     binding.pry
     @name = student_hash[:name]
+    @location = student_hash[:location]
   end
 
   def self.create_from_collection(students_array)
